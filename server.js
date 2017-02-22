3a1ddb39bc1421e1e56d77a68c82988727ebf6ee
Stas Lobanov <stas.lbv@gmail.com>
2017-02-22 11:40:11 +0200

    adding default server code

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+const rest_1 = require("./server/REST/rest");
+const db_1   = require("./server/DATABASE/db");
+const client = new rest_1.CRest();
+const db = new db_1.CDb(true);
+db.initialize().then(() => {
+    rest_1.CRest.pData = db;
+    client.Init();
+    client.Listen((process.env.PORT || 3000)).then(() => {
+        console.log('OnRestClientStartupComplete!');
+        
+    });
+}, (reject) => {
+    console.log('Server error, unable to set up database ...');
+});
